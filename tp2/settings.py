4f828df39f5dc43de81bf78dba1c92132dc06716
hamzamekni <96478203+hamzamekni@users.noreply.github.com>
2024-10-31 10:14:16 +0000

    Update settings.py

--- tp2/settings.py
+++ tp2/settings.py
@@ -45,12 +45,6 @@ INSTALLED_APPS = [
 ]
 CRISPY_TEMPLATE_PACK='bootstrap4'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'  # Replace with your SMTP server
-EMAIL_PORT = 587  # Common port for TLS
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'choeurproject@gmail.com'  # Your email address
-EMAIL_HOST_PASSWORD = 'oabw kzbc bghm mgeb'  # Your email password
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
